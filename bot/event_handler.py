01d73bb024a1a2b9b03a1041dfcbd5c524d24837
gbritt <gbritt91@gmail.com>
2016-11-02 09:00:14 -0400

    event

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -20,7 +20,7 @@ class RtmEventHandler(object):
             self._handle_by_type(event['type'], event)
 
 
-    def _handle_by_type(self, event_type):
+    def _handle_by_type(self, event_type, event):
         # See https://api.slack.com/rtm for a full list of events
 
         if event_type == 'error':
