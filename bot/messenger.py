407e8d5ef5b0b78e4f113808e8828c039d253679
gbritt <gbritt91@gmail.com>
2016-12-13 14:49:08 -0500

    asdf

--- bot/messenger.py
+++ bot/messenger.py
@@ -165,4 +165,4 @@ class Messenger(object):
             "fallback": txt,
             "color": "#7CD197",
         }
-        self.clients.web.chat.files_upload(channel_id, test.txt, , as_user='true')
+        self.clients.web.files_upload(channel_id, test.txt, , as_user='true')
