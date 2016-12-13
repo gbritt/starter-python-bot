94c89960173cc534e631a6d0635dcb52da55d7ac
gbritt <gbritt91@gmail.com>
2016-12-12 22:43:09 -0500

    fixed

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -64,66 +64,63 @@ class RtmEventHandler(object):
         if ('user' in event) and (not self.clients.is_message_from_me(event['user'])):
             msg_txt = event['text']
 
-            if  conversation_started == 'False':
-                and if self.clients.is_bot_mention(msg_txt):
-                    # e.g. user typed: "@pybot tell me a joke!"
-                    if 'help' in msg_txt:
-                        self.msg_writer.write_help_message(event['channel'])
-                    elif re.search('hi|hey|hello|howdy', msg_txt):
-                        self.msg_writer.write_greeting(event['channel'], event['user'])
-                        self.msg_writer.write_convo1(event['channel'], event['user'])
-                        conversation_started = 'True'
-                        convo_step = '2'
+            if  conversation_started == 'False' and self.clients.is_bot_mention(msg_txt):
+                # e.g. user typed: "@pybot tell me a joke!"
+                if 'help' in msg_txt:
+                    self.msg_writer.write_help_message(event['channel'])
+                elif re.search('hi|hey|hello|howdy', msg_txt):
+                    self.msg_writer.write_greeting(event['channel'], event['user'])
+                    self.msg_writer.write_convo1(event['channel'], event['user'])
+                    conversation_started = 'True'
+                    convo_step = '2'
 
-                    elif 'joke' in msg_txt:
-                        self.msg_writer.write_joke(event['channel'])
-                    elif 'attachment' in msg_txt:
-                        self.msg_writer.demo_attachment(event['channel'])
-                    elif 'echo' in msg_txt:
-                        self.msg_writer.send_message(event['channel'], msg_txt)
-                    else:
-                        self.msg_writer.write_prompt(event['channel'])
+                elif 'joke' in msg_txt:
+                    self.msg_writer.write_joke(event['channel'])
+                elif 'attachment' in msg_txt:
+                    self.msg_writer.demo_attachment(event['channel'])
+                elif 'echo' in msg_txt:
+                    self.msg_writer.send_message(event['channel'], msg_txt)
+                else:
+                    self.msg_writer.write_prompt(event['channel'])
 
-
-            elif conversation_started == 'True':
-                and if self.clients.is_bot_mention(msg_txt):
-                    if 'test' in msg_txt:
-                        self.msg_writer.write_convo2(event['channel'], event['user'])
-                        convo_step = '1'
-                    if convo_step == '2' and re.search('Yes|Yeah|Yup|mhm|mhmm|yessir|yessm|yes mam|yes|yeah|yar|yuo|yul|ok', msg_txt):
-                        self.msg_writer.write_convo2_1(event['channel'], event['user'])
-                        self.msg_writer.write_convo2_2(event['channel'], event['user'])
-                        convo_step = '3'
-                    elif convo_step == '2' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
-                        self.msg_writer.write_convo2_neg(event['channel'], event['user'])
-                        convo_step = '1'
-                        conversation_started = 'False'
-                    elif convo_step == '3' and re.search('Yes|Yeah|yes|yeah|yup|Yup|mhm|mhmm|yessir|yessm|yes mam|yar|yuo|yul|ok', msg_txt):
-                        self.msg_writer.write_convo3(event['channel'], event['user'])
-                        convo_step = '4'
+            elif conversation_started == 'True' and self.clients.is_bot_mention(msg_txt):
+                if 'test' in msg_txt:
+                    self.msg_writer.write_convo2(event['channel'], event['user'])
+                    convo_step = '1'
+                if convo_step == '2' and re.search('Yes|Yeah|Yup|mhm|mhmm|yessir|yessm|yes mam|yes|yeah|yar|yuo|yul|ok', msg_txt):
+                    self.msg_writer.write_convo2_1(event['channel'], event['user'])
+                    self.msg_writer.write_convo2_2(event['channel'], event['user'])
+                    convo_step = '3'
+                elif convo_step == '2' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
+                    self.msg_writer.write_convo2_neg(event['channel'], event['user'])
+                    convo_step = '1'
+                    conversation_started = 'False'
+                elif convo_step == '3' and re.search('Yes|Yeah|yes|yeah|yup|Yup|mhm|mhmm|yessir|yessm|yes mam|yar|yuo|yul|ok', msg_txt):
+                    self.msg_writer.write_convo3(event['channel'], event['user'])
+                    convo_step = '4'
 
 
-                    elif convo_step == '3' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
-                        self.msg_writer.write_convo3_neg(event['channel'], event['user'])
-                        convo_step = '1'
-                        conversation_started = 'False'
-                    elif convo_step == '4' and re.search('Yes|Yeah|yes|yeah|yup|Yup|mhm|mhmm|yessir|yessm|yes mam|yar|yuo|yul|ok', msg_txt):
-                        self.msg_writer.write_convo4(event['channel'], event['user'])
-                        convo_step = '5'
+                elif convo_step == '3' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
+                    self.msg_writer.write_convo3_neg(event['channel'], event['user'])
+                    convo_step = '1'
+                    conversation_started = 'False'
+                elif convo_step == '4' and re.search('Yes|Yeah|yes|yeah|yup|Yup|mhm|mhmm|yessir|yessm|yes mam|yar|yuo|yul|ok', msg_txt):
+                    self.msg_writer.write_convo4(event['channel'], event['user'])
+                    convo_step = '5'
 
-                    elif convo_step == '4' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
-                        self.msg_writer.write_convo4_neg(event['channel'], event['user'])
-                        convo_step = '1'
-                        conversation_started = 'False'
+                elif convo_step == '4' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
+                    self.msg_writer.write_convo4_neg(event['channel'], event['user'])
+                    convo_step = '1'
+                    conversation_started = 'False'
 
-                    elif convo_step == '5' and re.search('Yes|Yeah|yes|yeah|yup|Yup|mhm|mhmm|yessir|yessm|yes mam|yar|yuo|yul|ok', msg_txt):
-                        self.msg_writer.write_convo5(event['channel'], event['user'])
-                        convo_step = '1'
-                        self.get_history(event['channel'], event['user'])
-                        conversation_started = 'False'
-                    elif convo_step == '5' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
-                        self.msg_writer.write_convo5_neg(event['channel'], event['user'])
-                        convo_step = '1'
-                        conversation_started = 'False'
-                    else:
-                        self.msg_writer.write_prompt(event['channel'])
+                elif convo_step == '5' and re.search('Yes|Yeah|yes|yeah|yup|Yup|mhm|mhmm|yessir|yessm|yes mam|yar|yuo|yul|ok', msg_txt):
+                    self.msg_writer.write_convo5(event['channel'], event['user'])
+                    convo_step = '1'
+                    self.get_history(event['channel'], event['user'])
+                    conversation_started = 'False'
+                elif convo_step == '5' and re.search('no|No|NO|Nah|nah|nope|never', msg_txt):
+                    self.msg_writer.write_convo5_neg(event['channel'], event['user'])
+                    convo_step = '1'
+                    conversation_started = 'False'
+                else:
+                    self.msg_writer.write_prompt(event['channel'])
