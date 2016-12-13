914c156aca57076290773657afaf64afa148e990
gbritt <gbritt91@gmail.com>
2016-12-13 08:35:26 -0500

    d

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -52,6 +52,6 @@ class SlackClients(object):
         '''
         #history = self.rtm.api_call('channels.history', channel = channel_id)
         history = self.rtm.api_call('channels.list')
-        return history
+        return 'test2'
         #print self.rtm.api_call("chat.post_Message", as_user = "true:", #channel = channel_id, text = test)
         #self.trm.server.send_to_websocket
